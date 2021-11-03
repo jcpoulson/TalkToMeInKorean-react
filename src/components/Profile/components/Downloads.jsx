ca37ae5186000f50cfb9b0e3b1645914f5b8d3d0
Jonah <jonahpoulson@gmail.com>
2021-11-03 18:37:29 -0400

    Improved design and functionality on user profile component

--- src/components/Profile/components/Downloads.jsx
+++ src/components/Profile/components/Downloads.jsx
@@ -4,7 +4,7 @@ import { Row } from "antd";
 const Downloads = () => {
     return (
         <Row justify="center">
-            <h1>Whats uo bro this is the Downloads component</h1>
+            <h1>Whats up bro this is the Downloads component</h1>
         </Row>
     )
 }
